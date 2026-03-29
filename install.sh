cb03ebef07649ba72ca01c11362fd136b2b093a5
Abinaya Dinesh <abinayadinesh1@gmail.com>
2026-03-29 15:05:42 -0700

    easy install

---
+++ install.sh
@@ -0,0 +1,8 @@
+mv frame-bucket-api.service frame-bucket-pipeline.service stream-viewer.service /etc/systemd/system/
+echo "Moved system services to /etc/systemd/system"
+systemctl restart frame-bucket-pipeline.service
+echo "Started Pipeline"
+systemctl restart frame-bucket-api.service
+echo "Started API"
+systemctl restart stream-viewer.service
+echo "Started Stream Viewer"
